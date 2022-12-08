7556b674de139049c36d59501366cd24029e7dc8
Lucas reis <lucas.rs17neg@gmail.com>
2022-12-08 19:14:23 -0300

    atualizando problemas

--- lib/view/initialPage_screen.dart
+++ lib/view/initialPage_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:my_site_portfolio/components/InitialPage_screenItem.dart';
-
+import 'package:localization/localization.dart';
 class InitialPageScreen extends StatelessWidget {
   const InitialPageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return InitialPageScreenItem();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black12,
+        centerTitle: true,
+        title: Text('bridge_to_entry'.i18n()),
+      ),
+      body: InitialPageScreenItem(),
+    );
   }
 }
